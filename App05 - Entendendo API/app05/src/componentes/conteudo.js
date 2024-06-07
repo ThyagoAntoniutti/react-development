1387b842caa3abc5d4b39288cc350a3c8784d71c
Thyago <dev.thyago.capeleti@outlook.com>
2024-06-07 00:12:42 -0300

    update

---
+++ App05 - Entendendo API/app05/src/componentes/conteudo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function conteudo(){
+
+    return (
+
+        <>
+        <main>
+            <div>
+                <h1>Cadastro de Contatos</h1>
+            </div>
+        </main>
+        </>
+    )
+}
